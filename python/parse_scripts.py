9665f10a4aa5cc58b542ad6a36e40ace35cd54b9
Thomas Maucourt <maucourt.t@gmail.com>
2017-12-08 00:05:17 +0100

    massive update

--- python/parse_scripts.py
+++ python/parse_scripts.py
@@ -1,20 +1,33 @@
 import json
 
-with open("../fibo2.pl","r") as fp:
-    data=fp.read()
+from os import listdir
+from os.path import isfile, join
 
-dico={}
-maListe=data.split("\n",4)
-maListe.pop()
-tmp=""
+import sys
 
-for i in range(len(maListe)):
-    if i == 0:
-        dico[maListe[i][2:]]=[]
-        tmp=maListe[i][2:]
-    if i != 0:
-        dico[tmp].append(maListe[i][2:])
+# get path to parse
+PATH = str(sys.argv[1])
+files = [f for f in listdir(PATH) if isfile(join(PATH, f))]
 
-print dico
+id_scripts = dict()
+for current_file in files:
+    print(current_file)
+    with open(PATH+current_file,"r") as fp:
+        data = fp.read().split("\n", 5)
+        
+        language = data[0][2:].rstrip()
+        function = data[1][2:].rstrip()
+        consigne = data[2][2:].rstrip()
+        code = data[3][2:].rstrip()
+        img_path = data[4][2:].rstrip()
 
-json.dump(dico,open("../Nuit/tmp.json","w"), indent=4)
+    try:
+        try:
+            id_scripts[language][function].append([consigne, code, img_path])
+        except KeyError:
+            id_scripts[language][function] = [consigne, code, img_path]
+    except KeyError:
+        id_scripts[language] = {function : [[consigne, code, img_path]]}
+    
+
+json.dump(id_scripts, open(PATH+"tmp.json", "w"), indent = 4)
